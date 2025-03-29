9bb10900d494f166decc5d6aab152b8c17114613
rlhon <honruiling@gmail.com>
2025-03-29 15:11:13 -0700

    what we have so far

---
+++ src/screens/home/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./home.css";
+
+export default function Home() {
+    return (<div class = "text">
+        <p>Choose a few words to describe your mood today....
+            Maximum 3</p>
+        //set words here
+    </div>
+    );
+      
+}
+
+
+
